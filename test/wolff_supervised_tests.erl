e45b4ed9ff03d52ac23ef61beeba532ef5ba9a39
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-08-29 17:42:27 +0200

    fix(wolff_producers): Delete supervised producers before return error

--- test/wolff_supervised_tests.erl
+++ test/wolff_supervised_tests.erl
@@ -204,6 +204,19 @@ test_partition_count_refresh() ->
   {Partition1, _} = wolff:send_sync(Producers, [Msg], 3000),
   ?assertEqual(Partitions0, Partition1).
 
+non_existing_topic_test() ->
+  ClientId = atom_to_binary(?FUNCTION_NAME),
+  Topic = <<"non-existing-topic">>,
+  _ = application:stop(wolff), %% ensure stopped
+  {ok, _} = application:ensure_all_started(wolff),
+  ClientCfg = #{},
+  {ok, _ClientPid} = wolff:ensure_supervised_client(ClientId, ?HOSTS, ClientCfg),
+  ?assertMatch({error, _}, wolff:ensure_supervised_producers(ClientId, Topic, #{name => ?FUNCTION_NAME})),
+  ?assertMatch([], supervisor:which_children(wolff_producers_sup)),
+  ok = wolff:stop_and_delete_supervised_producers(ClientId, Topic, ?FUNCTION_NAME),
+  ok = wolff:stop_and_delete_supervised_client(ClientId),
+  ok.
+
 %% helpers
 wait_for_pid(F) ->
   Pid = F(),
